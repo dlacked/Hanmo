6f4a51edfbe9f2fbe916659c8412438fee3690fb
임창용 <imcy0104@gmail.com>
2025-01-14 14:37:09 +0000

    2월 수강신청 전 UI 재디자인

--- js/gnb.js
+++ js/gnb.js
@@ -1,63 +1,32 @@
-const $GNB = document.getElementsByClassName('gnb')[0];
-const $GNBclass = document.getElementsByClassName('show')[0];
-const $mobileHide = document.getElementsByClassName('mobileHide');
 const $headerNotice = document.getElementsByClassName('headerNotice')[0];
+const $HEADERNOTICELIST = document.getElementsByClassName('headerNoticeList')[0];
 var lastScroll, nowScroll;
 const now = new Date();
-
-checkDevice = () => {
-	const user = navigator.userAgent;
-	
-	if ( user.indexOf('iPhone') > -1 || user.indexOf('Android') > -1) {
-		for (var i = 0; i < $mobileHide.length; i++){
-			$mobileHide[i].style.display = 'none';
-		}
-	}
-}
 
 checkTime = () => {
 	if ((now.getHours() === 9 && now.getMinutes() >= 58) || (now.getHours() === 10 && now.getMinutes() <= 5)){
 		location.href = 'https://vsugangpractice.com/html/disable.html'
 	}
-	else if (now.getHours() === 9 && (now.getMinutes() >= 50)){
-		$headerNotice.style.display = 'flex';
+//	else if (now.getHours() === 9 && now.getMinutes() >= 40){
+	else if (true){
+		$headerNotice.style.top = '0px';
 	}
 }
 
-checkDevice();
 checkTime();
 
-window.addEventListener('scroll', function() {
-	lastScroll = nowScroll;
-	nowScroll = window.scrollY;
-	if (nowScroll > 0 && nowScroll != undefined) {
-		$GNB.style.color = 'black';
-		$GNB.style.background = 'white';
-	} else{
-		$GNB.style.color = 'white';
-		$GNB.style.background = '#000d18';
-	}
-	if (nowScroll > lastScroll) {
-		$GNBclass.classList.add('hide');
-		$GNBclass.classList.remove('show');
+
+var headerNoticeCnt = true;
+
+const setHeaderNotice = setInterval(() => {
+	if (headerNoticeCnt === true) {
+		headerNoticeBottom = 100;
+		headerNoticeCnt = false;
 	} else {
-		$GNBclass.classList.add('show');
-		$GNBclass.classList.remove('hide');
-		$GNB.style.color
+		headerNoticeBottom = 44;
+		headerNoticeCnt = true;
+		
 	}
-})
-
-$GNB.addEventListener('mouseenter', function() {
-	if (nowScroll === 0 || nowScroll === undefined) {
-		$GNB.style.background = 'white';
-		$GNB.style.color = 'black';
-	}
-})
-
-$GNB.addEventListener('mouseleave', function() {
-	if (nowScroll === 0 || nowScroll === undefined) {
-		$GNB.style.background = '#000d18';
-		$GNB.style.color = 'white';
-	}
-})
+	$HEADERNOTICELIST.style.bottom = `${headerNoticeBottom}px`;
+}, 5000);
 
--- js/script.js
+++ js/script.js
@@ -1,39 +1,25 @@
 const $NOTICELIST = document.getElementsByClassName('noticeList')[0];
 const $NOTICELISTQUERY = document.querySelector(".noticeList");
-const $GRID1 = document.getElementsByClassName('grid1')[0];
-const $GRID2 = document.getElementsByClassName('grid2')[0];
-const $GRID3 = document.getElementsByClassName('grid3')[0];
-const $MOVE1 = document.getElementsByClassName('move1')[0];
-const $MOVE2 = document.getElementsByClassName('move2')[0];
-const $MOVE3 = document.getElementsByClassName('move3')[0];
+
 /*리스트에 공지사항 내용 적어주세요.*/
-const noticeList = ['25학번의 새로운 시작을 응원해요!', '2024년 11월, 한모 UI 업데이트 완료', '한국외국어대학교 70주년 (1954년 개교)']
+const noticeList = ['한국외대 25학번 학우들의 첫 학기를 응원합니다!', '2025년 1월, 한모 UI 업데이트 완료', '한국외국어대학교 70주년 (1954년 개교)']
 
 for (var i = noticeList.length-1; i >= 0; i--) {
 	$NOTICELISTQUERY.insertAdjacentHTML('afterbegin', `<li>${noticeList[i]}</li>`)
 }
 
-var cnt = 79;
+var cnt = 1;
 
 const setNotice = setInterval(() => {
-	if (noticeList.length * 79 < cnt + 79) {
-		cnt = 0;
+	if (cnt === 1) {
+		$NOTICELIST.style.bottom = '77px';
+		cnt++;
+	} else if (cnt === 2) {
+		$NOTICELIST.style.bottom = '154px';
+		cnt++;
+	} else{
+		$NOTICELIST.style.bottom = '0px';
+		cnt=1;
 	}
-	$NOTICELIST.style.bottom = `${cnt}px`;
-	cnt += 79;
 }, 3000);
 
-/*
-const setGrid = setInterval(() => {
-	$GRID1.style.gridTemplateRows = `1fr ${Number(Math.random())*50}%`;
-	$GRID2.style.gridTemplateRows = `1fr ${Number(Math.random())*50}%`;
-	$GRID3.style.gridTemplateRows = `1fr ${Number(Math.random())*50}%`;
-}, 2000);
-*/
-
-/*
-const setGrid = setInterval(() => {
-	$GRID1.style.gridTemplateRows = `1fr ${Number(Math.random())*100}%`;
-	$GRID2.style.gridTemplateRows = `1fr ${Number(Math.random())*100}%`;
-	$GRID3.style.gridTemplateRows = `1fr ${Number(Math.random())*100}%`;
-}, 2000);*/
