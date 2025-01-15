21e275ab1f6d4b8458c74096f726e3ef12d2452c
임창용 <imcy0104@gmail.com>
2025-01-15 14:35:48 +0000

    index 페이지 버그 수정

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const $SUGANGBUTTON = document.getElementsByClassName('button')[0];
 const $JEHAN = document.getElementsByClassName('jehan')[0];
 
 const nowTime = new Date();
+const user = navigator.userAgent;
 
 /*리스트에 공지사항 내용 적어주세요.*/
 const noticeList = ['한국외대 25학번 학우들의 첫 학기를 응원합니다!', '2025년 1월, 한모 UI 업데이트 완료', '한국외국어대학교 70주년 (1954년 개교)']
