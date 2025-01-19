a51edbd5ad3fb1af5f22f16e4b469d536ac51fe7
임창용 <imcy0104@gmail.com>
2025-01-19 01:31:50 +0000

    이용 제한 디바이스 버그 수정

--- js/checkTime.js
+++ js/checkTime.js
@@ -2,8 +2,10 @@
 const now = new Date();
 
 check = () => {
+	console.log('실행')
 	//if(true){
-	if ((now.getHours() === 9 && now.getMinutes() >= 58) || (now.getHours() === 10 && now.getMinutes() <= 10)){
+	if (((now.getHours() === 9 && now.getMinutes() >= 58) || (now.getHours() === 10 && now.getMinutes() <= 10)) || localStorage.getItem('deviceInfo') === 'false'){
+		console.log('스마트폰')
 		location.href = 'https://vsugangpractice.com/';
 	}
 }
