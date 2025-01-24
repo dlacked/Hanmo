c8a113c603021b8a1cc70b13cc9669331b226223
임창용 <imcy0104@gmail.com>
2025-01-24 15:01:12 +0000

    긴급 버그 수정

--- js/practice.js
+++ js/practice.js
@@ -1,14 +1,15 @@
 const practiceValue = localStorage.getItem('practiceSetter');
 const practiceMode = document.getElementById('practiceMode');
 
+//시간 측정값을 초기화시킴
 localStorage.setItem('TIMEtime', '0.000');
 localStorage.setItem('SELECTtime', '0.000');
 
-if (practiceValue == 3){
+if (practiceValue == 3){ //SELECT MODE
 	practiceMode.innerText = '연습 모드: SELECT MODE'
-	document.getElementById('timer').innerHTML = '<span>X</span>'
+	document.getElementById('timer').innerHTML = '<span>10:00:00</span>'
 
-} else{
+} else{ //TIME MODE
 	practiceValue == 2 ? practiceMode.innerText = '연습 모드: TIME MODE' : practiceMode.innerText = '연습 모드: BASIC MODE';
 	
 	document.getElementById('timer').innerHTML = '<span id="hours">09</span>:<span id="minutes">59</span>:<span id = "seconds">50</span>'
@@ -21,22 +22,22 @@ if (practiceValue == 3){
 	let eleHours = document.getElementById('hours');
 	let interval;
 	let clickedSugang = document.getElementById('sugang');
-	var startTime, duringTime;
+	let startTime, duringTime;
 
 	window.onload = function deviceCheck(){
-		interval = setInterval(startTimer, 1000);
+		interval = setInterval(timer, 1000);
 		setTimeout(function(){
-			startTime = new Date()
-		}, 10000)
+			startTime = new Date();
+		}, 10000);
 	}
 
-	clickedSugang.onclick = function(){    //스톱워치, register 총합 8번 눌렸을 시 스톱워치 종료
+	clickedSugang.onclick = function(){ //스톱워치, register 총합 8번 눌렸을 시 스톱워치 종료
 		var endTime = new Date()
 		if (isNaN(endTime - startTime) == true){
 			alert('수강신청 기간이 아닙니다.');
 		} else {
 			duringTime = (endTime - startTime) / 1000
-			if (practiceValue == 1){
+			if (practiceValue == 1){ //BASIC MODE
 				//duringTime = (endTime - startTime) / 1000 //스톱워치 종료시간에서 페이지 입장 시간을 뺌
 				localStorage.setItem('TIMEtime', duringTime);
 				location.replace('./selectZone.html');
@@ -49,7 +50,7 @@ if (practiceValue == 3){
 		}
 	}
 
-	function startTimer(){
+	function timer(){
 		seconds++;
 		if(seconds <= 9) eleSeconds.innerText = '0' + seconds;
 		else eleSeconds.innerText = seconds;
@@ -70,4 +71,5 @@ if (practiceValue == 3){
 			eleMinutes.innerText = '00';
 		}
 	}
+
 }
