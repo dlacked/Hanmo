dbba7a76ef603cb898daecbb4827ec7fa401879b
임창용 <imcy0104@gmail.com>
2025-01-24 12:59:54 +0000

    JS 1차 간략화 (파일 통합)

--- js/hanmoSys.js
+++ js/hanmoSys.js
@@ -1,13 +1,23 @@
 /*index.html은 index.js에서 환경 체킹함*/
 const now = new Date();
 
-check = () => {
-	
-	//if (((now.getHours() === 20 && now.getMinutes() >= 58) || (now.getHours() === 21 && now.getMinutes() <= 10)) || localStorage.getItem('deviceInfo') === 'false'){
-	//if(true){
+const title = document.getElementsByClassName('title')[0];
+
+restrictHanmo = () => {
 	if (((now.getHours() === 9 && now.getMinutes() >= 58) || (now.getHours() === 10 && now.getMinutes() <= 10)) || localStorage.getItem('deviceInfo') === 'false'){
 		location.href = 'https://vsugangpractice.com/';
 	}
 }
 
-check();
+setSemester = () => {
+	let hakgi = 1
+	if (now.getMonth() >= 2 && now.getMonth() <= 7){
+		hakgi = 2;
+	}
+	title.innerText = `${now.getFullYear()}-${hakgi}학기`;
+
+}
+
+
+setSemester();
+restrictHanmo();
