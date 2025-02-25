44b41856d2169f2abba3cbc8c8ccd830e96b7b40
임창용 <imcy0104@gmail.com>
2025-02-26 00:25:17 +0900

    보고서 값 출력 버그 수정

--- js/result.js
+++ js/result.js
@@ -5,17 +5,18 @@ timeCheck = () => {
 	var subjectValue = localStorage.getItem('subjectValue');
 	if (localStorage.getItem('practiceSetter') == 2){
 		subjectValue = 0;
-	}
+	} 
+	
 	const TIMEtime = Number(localStorage.getItem('TIMEtime')).toFixed(3);
 	const SELECTtime = Number(localStorage.getItem('SELECTtime')-((2*subjectValue)+2)).toFixed(3);
 	const TOTALtime = (Math.round((Number(TIMEtime) + Number(SELECTtime))*1000)/1000).toFixed(3);
 	
 	$timeBox.insertAdjacentHTML('afterbegin', `<p>연습한 과목 수 ${subjectValue}개</p>
 					<p>TIME 부분 소요 시간<span>${TIMEtime}초</span></p>
-					<p>SELECT 부분 소요 시간<span>${SELECTtime}초</span></p>
+					<p>SELECT 부분 소요 시간<span>${subjectValue > 0 ? SELECTtime : '0.000'}초</span></p>
 					<p class='caption'>접속 대기 시간은 소요 시간에서 제외돼요.</p>`);
 	$resultBox.insertAdjacentHTML('afterbegin', `
-					<p>총 소요 시간<span>${TOTALtime}초</span></p>`);
+					<p>총 소요 시간<span>${subjectValue > 0 ? TOTALtime : TIMEtime}초</span></p>`);
 }
 
 timeCheck();
